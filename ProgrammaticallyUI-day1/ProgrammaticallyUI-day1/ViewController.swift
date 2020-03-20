196c2f081f8f5cc7a4e56b922b289ce8154fdd30
margulan9 <sugirbaymargulan@gmail.com>
2020-03-20 19:39:54 +0600

    initial commit

---
+++ ProgrammaticallyUI-day1/ProgrammaticallyUI-day1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticallyUI-day1
+//
+//  Created by Sugirbay Margulan on 3/20/20.
+//  Copyright © 2020 Sugirbay Margulan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
