222dce4c85b84c03d6d6835971080ed6bb0e6dc3
margulan9 <sugirbaymargulan@gmail.com>
2020-04-03 20:06:53 +0600

    no storyboard

---
+++ NoStoryboard/NoStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStoryboard
+//
+//  Created by Sugirbay Margulan on 3/31/20.
+//  Copyright © 2020 Sugirbay Margulan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
